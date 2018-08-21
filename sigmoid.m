4de8578cb878ab48d0ec469ea4ffdbdcfdf930a8
JeetBhattachariya <jeet.bhattachariya@gmail.com>
2018-08-21 15:46:26 +1000

    logistic regression cost and grad
    test

---
+++ sigmoid.m
@@ -0,0 +1,23 @@
+function g = sigmoid(z)
+%SIGMOID Compute sigmoid function
+%   g = SIGMOID(z) computes the sigmoid of z.
+
+% You need to return the following variables correctly 
+g = zeros(size(z));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the sigmoid of each value of z (z can be a matrix,
+%               vector or scalar).
+row=size(z,1);col=size(z,2);
+for i=1:row
+  for j=1:col
+    g(i,j)=e^z(i,j)/(1+e^z(i,j));
+	j=j+1;
+ end
+ i=i+1;
+end
+
+
+% =============================================================
+
+end
